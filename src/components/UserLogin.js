e8aaa1a02e6cea02700538a4b90798cfb4818d4c
Tony Robertson <tonyr729@gmail.com>
2018-05-24 17:17:03 -0600

    Add try/catch to UserLogin

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -4,11 +4,17 @@ import firebase from '../firebase';
 const UserLogin =() => {
 
   const googleLogin = async () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    const response = await firebase.auth().signInWithPopup(provider);
-    const user = response.user;
-    console.log(user);
-  }
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const response = await firebase.auth().signInWithPopup(provider);
+      const user = response.user;
+    } catch(error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+      const errorInfo = { errorCode, errorMessage, email, credential};
+    };
 
   return (
     <button onClick={()=> googleLogin()}>Login with Google</button>
