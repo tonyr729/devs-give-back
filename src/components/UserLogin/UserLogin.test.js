b218feb6e42e57ae6e9a23f246cf781ff3b783f1
Tony Robertson <tonyr729@gmail.com>
2018-05-26 13:48:15 -0600

    Add tests for UserLogin

--- src/components/UserLogin/UserLogin.test.js
+++ src/components/UserLogin/UserLogin.test.js
@@ -1,11 +1,29 @@
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import React from 'react';
-import { UserLogin } from './UserLogin'
+import { UserLogin } from './UserLogin';
+import firebase from '../../firebase/firebase'
+jest.mock('../../firebase/firebase')
 
 describe('UserLogin', () => {
   it('should match snapshot', () => {
-    const userLogin = shallow(<UserLogin  />)
+    const userLogin = shallow(<UserLogin  />);
 
     expect(userLogin).toMatchSnapshot();
   });
+
+  describe('googleLogin', () => {
+    let userLogin;
+    beforeEach(() => {
+      userLogin = shallow(<UserLogin />);
+    });
+
+    it('should call the expected methods of firebase', () => {
+      userLogin.find('button').simulate('click');
+
+      expect(firebase.auth().signInWithPopup).toHaveBeenCalled()
+      expect(firebase.auth.GoogleAuthProvider).toHaveBeenCalled()
+
+    });
+  });
+  
 });
