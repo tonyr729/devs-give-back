8478f81ec04e38c697cdbad9263f75bf4af1765e
Tony Robertson <tonyr729@gmail.com>
2018-05-29 00:18:55 -0600

    Add better logic to apiCall

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -2,10 +2,14 @@ import firebase from '../firebase/firebase';
 
 export const findMatchingProblem = async (userID) => {
   const response = await firebase.database().ref('/Problems/').once('value');
+  let problem = null;
   if (response.val()) {
     console.log(response.val())
+    console.log(userID)
     const problems = response.val();
     const matchingID = Object.keys(problems).find(problem => problems[problem].clientID === userID)
-    return problems[matchingID]
+    problem = problems[matchingID];
+    console.log(problem)
   }
+  return problem;
 }
