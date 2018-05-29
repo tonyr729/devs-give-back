67d48ad86f420800b45d489f4e138e0e84d0de9f
Tony Robertson <tonyr729@gmail.com>
2018-05-29 00:17:38 -0600

    Make PriorProblems a container
    Made PriorProblems a container and sets state tree with info from database.

---
+++ src/containers/PriorProblem/PriorProblem.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { findMatchingProblem } from '../../helpers/apiCalls';
+import { connect } from 'react-redux';
+import {createProblemTitle, createProblemBody, createProblemClient } from '../../actions/actions';
+import './PriorProblem.css'
+
+class PriorProblem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      problem: {}
+    }
+  }
+
+  componentDidMount = async () => {
+    const problem = await findMatchingProblem(this.props.user.id);
+    console.log(problem)
+    this.setState({
+      problem: problem
+    })
+  }
+  
+  handleClick = () => {
+    if (this.state.problem){
+      this.props.createProblemClient(this.state.problem.clientID)
+      this.props.createProblemTitle(this.state.problem.title)
+      this.props.createProblemBody(this.state.problem.body)
+      this.props.history.push("/client-profile");
+    } else {
+      this.props.history.push("/client-login")
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p className='sorry-text'>Sorry,</p>
+        <p className='info-message'> can only have one listed problem at a time.</p>
+          <button onClick={ this.handleClick }>Go to my profile</button>
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user 
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  createProblemClient: (client) => dispatch(createProblemClient(client)),
+  createProblemTitle: (title) => dispatch(createProblemTitle(title)),
+  createProblemBody: (body) => dispatch(createProblemBody(body))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PriorProblem);
