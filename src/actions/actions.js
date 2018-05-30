9ed5b70f62d10f626d417b59bd5846e5e3045ec5
Tony Robertson <tonyr729@gmail.com>
2018-05-29 23:25:03 -0600

    Add actions and reducers for DevLogin

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -8,6 +8,16 @@ export const clientError = (error) => ({
   error
 });
 
+export const signInDev = (user) => ({
+  type: 'SIGN_IN_DEV',
+  user
+});
+
+export const devError = (error) => ({
+  type: 'DEV_ERROR',
+  error
+});
+
 export const createProblemClient = (client) => ({
   type: 'CREATE_PROBLEM_CLIENT',
   client
---
+++ src/reducers/devLoginReducer.js
@@ -0,0 +1,19 @@
+const initalState = {};
+
+export const devLoginReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case 'SIGN_IN_DEV':
+      return action.user;
+    default:
+      return state;
+  }
+}
+
+export const devErrorReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case 'DEV_ERROR':
+      return action.error;
+    default:
+      return state;
+  }
+}
