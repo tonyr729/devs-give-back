bfbaaa47493957961c54c8f2bb29bcdda0e857d3
Tony Robertson <tonyr729@gmail.com>
2018-05-29 23:26:50 -0600

    Add reducers to root for dev actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { clientLoginReducer, clientErrorReducer } from './clientLoginReducer';
+import { devLoginReducer, devErrorReducer } from './devLoginReducer';
 import { problemTitleReducer, problemBodyReducer, problemClientReducer } from './problemReducer';
 
 export const rootReducer = combineReducers({
-  user: clientLoginReducer,
+  client: clientLoginReducer,
   clientError: clientErrorReducer,
+  dev: devLoginReducer,
+  devError: devErrorReducer,
   problemClient: problemClientReducer,
   problemTitle: problemTitleReducer, 
   problemBody: problemBodyReducer
