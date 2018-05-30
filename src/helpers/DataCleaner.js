cd24c20f7531275fede5ad9389f31b137b30311e
Tony Robertson <tonyr729@gmail.com>
2018-05-29 23:26:03 -0600

    Add functions to helper files to clean up login
    Added data cleaner for error object, and moved gitHubLogin into API file.

--- src/helpers/DataCleaner.js
+++ src/helpers/DataCleaner.js
@@ -6,6 +6,21 @@ class DataCleaner {
     photoURL: client.photoURL,
     apiKey: client.apiKey
   });
+
+  cleanDevLogin =(dev, token) => ({
+    id: dev.uid,
+    name: dev.displayName,
+    photoURL: dev.photoURL,
+    token: token
+  });
+
+  cleanError = (error) => ({
+    code: error.code,
+    message: error.message,
+    email: error.email,
+    credential: error.credential
+  })
+
 }
 
 export default DataCleaner;
--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -1,15 +1,23 @@
 import firebase from '../firebase/firebase';
 
+
+export const gitHubLogin = async () => {
+  const provider = new firebase.auth.GithubAuthProvider()
+  // provider.addScope('repo');
+  const response = await firebase.auth().signInWithPopup(provider);
+  const token = response.credential.accessToken;
+  const user = response.user;
+  return {user, token}
+}
+
+
 export const findMatchingProblem = async (userID) => {
   const response = await firebase.database().ref('/Problems/').once('value');
   let problem = null;
   if (response.val()) {
-    console.log(response.val())
-    console.log(userID)
     const problems = response.val();
     const matchingID = Object.keys(problems).find(problem => problems[problem].clientID === userID)
     problem = problems[matchingID];
-    console.log(problem)
   }
   return problem;
 }
