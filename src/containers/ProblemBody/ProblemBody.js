11e50f9671ae956fa87ed31e604acaea2fe6ff1e
Tony Robertson <tonyr729@gmail.com>
2018-06-04 16:26:00 -0600

    Add autofocus to inputs in problem creation

--- src/containers/ProblemBody/ProblemBody.js
+++ src/containers/ProblemBody/ProblemBody.js
@@ -43,7 +43,7 @@ class ProblemBody extends Component {
       <div className='client-background'>
         <p className='welcome'>Sounds great {firstName},</p>
         <p className='body-instructions'>Now give us some more detail</p>
-        <textarea className='body-input' placeholder='Looking for ...' maxLength='400' value={this.state.input} onChange={this.handleInputChange}/>
+        <textarea className='body-input' placeholder='Looking for ...' maxLength='400' value={this.state.input} onChange={this.handleInputChange} autoFocus/>
         <div className="button-container">
           <button className='previous-button' onClick={()=> this.props.history.push("/problem-title")} ><img className='left-arrow-img' src={leftArrow} /></button>
           <button className='next-button' onClick={this.handleSubmit} ><img className='right-arrow-img' src={rightArrow} /></button>
