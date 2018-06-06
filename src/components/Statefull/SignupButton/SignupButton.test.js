abd3a715de2ffd95e8f0fcdd3431b41227a9cf74
Tony Robertson <tonyr729@gmail.com>
2018-06-05 18:42:21 -0600

    Add tests for SignupButton

---
+++ src/components/Statefull/SignupButton/SignupButton.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import SignupButton from './SignupButton';
+
+describe('SignupButton', () => {
+  let wrapper;
+  let mockHandleSignup;
+
+  beforeEach(() => {
+    mockHandleSignup = jest.fn();
+    wrapper = shallow(<SignupButton handleSignup={mockHandleSignup} clientID={'clientID'}  />);
+  })
+
+  it('should match snapshot', () => {
+
+    expect(wrapper).toMatchSnapshot();
+
+  });
+  
+  describe('handleClick', () => {
+    it('should call prop methods and values', () => {
+      wrapper.instance().handleClick()
+
+      expect(mockHandleSignup).toHaveBeenCalled();
+    });
+  });
+});
